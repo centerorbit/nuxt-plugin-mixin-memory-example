92fd5146661be7623430fe887696bb2ba2ed7b61
Andy LeTourneau <centerorbit@gmail.com>
2020-05-25 06:29:41 -0500

    An example setup with memory exhaustion using Nuxt SSR and Vue global mixin plugins

---
+++ plugins/globals.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import { isEmpty } from 'lodash'
+
+Vue.mixin({
+  methods: {
+    elipsify (subject, maxLength = 40) {
+      if (subject.length < maxLength) {
+        return subject
+      } else {
+        return subject.substring(0, (maxLength - 4)) + ' ...'
+      }
+    },
+    isEmpty (item) {
+      return isEmpty(item)
+    }
+  }
+})
